8d5eaf1551b088d1743abd728be1aaa635c01a3b
Patrick <patrick.krisko@gmail.com>
2019-10-06 18:59:10 -0500

    Slight refactor atoi, format

--- js/myAtoi.js
+++ js/myAtoi.js
@@ -1,34 +1,46 @@
 /**
+ * Runtime: 56 ms, faster than 99.80% of JavaScript online submissions for
+ * String to Integer (atoi).
+ *
+ * Memory Usage: 36.7 MB, less than 14.29% of JavaScript online submissions for
+ * String to Integer (atoi).
+ */
+
+const MAX_INT = Math.pow(2, 31) - 1;
+const MIN_INT = Math.pow(-2, 31);
+
+/**
  * @param {string} str
  * @return {number}
  */
-const MAX_INT = Math.pow(2, 31) -1;
-const MIN_INT = Math.pow(-2, 31);
-
 const myAtoi = str => {
     let foundNonWhitespace = false;
-    let num = '';
+    let num = '', curr;
     // Creating these variables helps with performance.
     const n = str.length;
     const plusMinusRe = /[+-]/;
     const digitRe = /\d/;
+    // For up to N, length of @param str.
     for (let idx = 0; idx < n; idx++) {
-        const curr = str[idx];
+        curr = str[idx];
         if (curr !== ' ') {
-            foundNonWhitespace = true;
-            // If digit, or if sign (+-) is first nonWhiteSpace char.
-            if (digitRe.test(curr)
-                || (plusMinusRe.test(curr) && (!plusMinusRe.test(str[idx - 1]) || idx === 0))) {
-                num += curr;
-            } else { // Not valid addition to number.
-                break;
-            }
+        foundNonWhitespace = true;
+        // If digit, or if sign (+-) is first nonWhiteSpace char, then add it.
+        if (
+            digitRe.test(curr) ||
+            (plusMinusRe.test(curr) &&
+            (!plusMinusRe.test(str[idx - 1]) || idx === 0))
+        ) {
+            num += curr;
+        } else {
+            break;
+        }
         // Trailing whitespace, exit loop.
         } else if (curr === ' ' && foundNonWhitespace) {
             break;
         }
     }
-    if (num === '' || num === '-' || num === '+') {
+    if (num === '' || /^[+-]{1}$/.test(num)) {
         return 0;
     }
     num = Number.parseInt(num);
